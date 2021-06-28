85db338164d549edcc347e7bda07119b0129798f
Ilya <arzaevilya@gmail.com>
2021-06-28 21:00:32 +0200

    first commit

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Leave()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.LogFormat("Player {0} entered room", newPlayer.NickName);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.LogFormat("Player {0} left room", otherPlayer.NickName);
+    }
+}
---
+++ Scripts/LobbyManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public Text LogText;
+    
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player " + Random.Range(1000, 9999);
+        Log("Player's name is set to " + PhotonNetwork.NickName);
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log("Connected to Master");
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log("Joined the room");
+
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    private void Log(string message)
+    {
+        Debug.Log(message);
+        LogText.text += "\n";
+        LogText.text += message;
+    }
+}
